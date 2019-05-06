4ce81b052c6eb2c223c56674d2cff939083fbc67
s5907620 <s0953780241@gmail.com>
2019-05-07 00:57:31 +0800

    ALU_1bit
    slt未完成

--- ALU.v
+++ ALU.v
@@ -1,14 +1,14 @@
 // Test
 `timescale 1ns/1ns
-module ALU( dataA, dataB, Signal, dataOut );
+module ALU( dataA, dataB, Signal, dataOut, binvert );
 
-    input [31:0]  dataA ;
-    input [31:0]  dataB ;
+    input   dataA ;
+    input   dataB ;
     input [5:0]   Signal ;
-    output [31:0] dataOut ;
-    wire [31:0]   rcaB ;
-    wire [31:0]   andOut, orOut, rcaOut, sltOut;
-	wire          binvert, carryOut;
+    output  dataOut ;
+    wire    rcaB ;
+    wire    andOut, orOut, FAout, sltOut;
+	wire    binvert, carryOut;
 
 	parameter AND = 6'b100100;
     parameter OR  = 6'b100101;
@@ -16,17 +16,17 @@ module ALU( dataA, dataB, Signal, dataOut );
     parameter SUB = 6'b100010;
     parameter SLT = 6'b101010;
 
-    and and32[31:0] (andOut, dataA, dataB);
-    or or32[31:0] (orOut, dataA, dataB);
-    assign sltOut = dataA < dataB;
+    and tand (andOut, dataA, dataB);
+    or tor (orOut, dataA, dataB);
+    // slt
 
     assign binvert = (Signal == SUB) ? 1'b1 : 1'b0;
-    xor invert[31:0] (rcaB, dataB, binvert);
-    RCA RCA1(dataA, rcaB, rcaOut, carryOut, binvert);
+    xor invert (rcaB, dataB, binvert);
+    FA tFA (dataA, rcaB, c, carryOut, FAout);
 
     assign dataOut = (Signal == AND) ? andOut :
                      (Signal == OR) ? orOut :
-                     (Signal == ADD || Signal == SUB) ? rcaOut :
+                     (Signal == ADD || Signal == SUB) ? FAout :
                      (Signal == SLT) ? sltOut :
                      32'b0;
 endmodule
