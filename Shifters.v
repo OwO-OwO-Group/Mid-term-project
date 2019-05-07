83063a2e5fa17f0cbe9e21ca79980fba8c64f275
s5907620 <s0953780241@gmail.com>
2019-05-07 16:34:08 +0800

    Shift Test
    2 ** 0層 第1bit 測試

--- Shifters.v
+++ Shifters.v
@@ -6,15 +6,16 @@ module Shifters( dataA, dataB, Signal, dataOut );
     input [5:0] Signal;
     output [31:0] dataOut;
 
-    parameter SRL = 6'b000010;
-
-    genvar mux, level;
+    parameter SLL = 6'b000000;
 
     wire [31:0] wires [5:0];
 
-    assign wires[0] = dataA;
-    assign dataOut = (Signal == SRL && (dataB[31:5]) == 25'b0  ) ?  wires[5] : 32'b0 ;
+    assign wires[0] = dataA; // 第一層讀入A
+    assign dataOut = (Signal == SLL && (dataB[31:5]) == 27'b0 ) ? wires[5] : 32'b0 ; // 檢查訊號以及移位值是否正確
 
+    assign wires[1][0] = dataB[0] ? 1'0 : dataA[0];
+	
+	/*
     for (level = 0; level < 5; level = level + 1)
     begin
         for (mux = 0; mux < 32; mux = mux + 1)
@@ -26,5 +27,5 @@ module Shifters( dataA, dataB, Signal, dataOut );
                 assign wires[level + 1][mux] = dataB[level] ? 1'b0 : wires[level][mux];
         end
     end
-
+	*/
 endmodule
