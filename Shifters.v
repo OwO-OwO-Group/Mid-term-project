ead05f4b97c46c8f0c554b0af360db2c90c718c1
s5907620 <s0953780241@gmail.com>
2019-05-07 19:22:29 +0800

    shift

--- Shifters.v
+++ Shifters.v
@@ -13,8 +13,8 @@ module Shifters( dataA, dataB, Signal, dataOut );
     assign wires[0] = dataA; // 第一層讀入A
     assign dataOut = (Signal == SLL && (dataB[31:5]) == 27'b0 ) ? wires[5] : 32'b0 ; // 檢查訊號以及移位值是否正確
 
-	// 第一層 8bits test
-    assign wires[1][0] = dataB[0] ? 1'0      : dataA[0];
+	// 第一層 8bits test 左移1bit
+    assign wires[1][0] = dataB[0] ? 1'b0     : dataA[0];
 	assign wires[1][1] = dataB[0] ? dataA[0] : dataA[1];
 	assign wires[1][2] = dataB[0] ? dataA[1] : dataA[2];
 	assign wires[1][3] = dataB[0] ? dataA[2] : dataA[3];
@@ -22,6 +22,27 @@ module Shifters( dataA, dataB, Signal, dataOut );
 	assign wires[1][5] = dataB[0] ? dataA[4] : dataA[5];
 	assign wires[1][6] = dataB[0] ? dataA[5] : dataA[6];
 	assign wires[1][7] = dataB[0] ? dataA[6] : dataA[7];
+	
+	// 第二層 8bits test 左移2bits
+	assign wires[2][0] = dataB[1] ? 1'b0        : wires[1][0];
+	assign wires[2][1] = dataB[1] ? 1'b0        : wires[1][1];
+	assign wires[2][2] = dataB[1] ? wires[1][0] : wires[1][2];
+	assign wires[2][3] = dataB[1] ? wires[1][1] : wires[1][3];
+	assign wires[2][4] = dataB[1] ? wires[1][2] : wires[1][4];
+	assign wires[2][5] = dataB[1] ? wires[1][3] : wires[1][5];
+	assign wires[2][6] = dataB[1] ? wires[1][4] : wires[1][6];
+	assign wires[2][7] = dataB[1] ? wires[1][5] : wires[1][7];
+	
+	
+	// 第三層
+	assign wires[3][0] = dataB[2] ? 1'b0        : wires[2][0];
+	assign wires[3][1] = dataB[2] ? 1'b0        : wires[2][1];
+	assign wires[3][2] = dataB[2] ? 1'b0        : wires[2][2];
+	assign wires[3][3] = dataB[2] ? 1'b0        : wires[2][3];
+	assign wires[3][4] = dataB[2] ? wires[2][0] : wires[2][4];
+	assign wires[3][5] = dataB[2] ? wires[2][1] : wires[2][5];
+	assign wires[3][6] = dataB[2] ? wires[2][2] : wires[2][6];
+	assign wires[3][7] = dataB[2] ? wires[2][3] : wires[2][7];
 	
 	/*
     for (level = 0; level < 5; level = level + 1)
